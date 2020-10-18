668f508fd8847fe51cc9774d5d2eadceefea686c
Kim Zuniga <kim.eli.zuniga@gmail.com>
2020-10-18 15:57:13 -0400

    change port

--- app.js
+++ app.js
@@ -109,6 +109,10 @@ if (port == null || port == "") {
   port = 4000;
 }
 
-app.listen(port, () => {
-    console.log("Server started " + port);
-});
+// app.listen(port, () => {
+//     console.log("Server started " + port);
+// });
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
