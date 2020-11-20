9c9eb3c0decb1acdaa9db452e02092aa9a216383
Kim Zuniga <kim.eli.zuniga@gmail.com>
2020-11-20 11:22:52 -0500

    Testing checklists

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,58 +1,13 @@
-<!-- <% let dueToday = 0; %>
-<% let pastDues = 0; %>
-
-<% let todaysDate = new Date() %>
-<% for(i = 0; i < taskOnes.length; i++) {%>
-<%        let taskone = taskOnes; %>
-<%      if(taskone[i].date === todaysDate) {%>
-<%          dueToday++; %>
-<%      } %>
-<%      if(taskOnes[i].date < todaysDate) {%>
-<%          pastDues++; %>
-<%      } %> 
-<% } %>
-
-<% for(i = 0; i < taskTwos.length; i++) {%>
-<%      if(taskTwos[i].date === todaysDate) {%>
-<%          dueToday++; %>
-<%      } %>
-<%      if(taskTwos[i].date < todaysDate.setHours(0,0,0,0)) {%>
-<%          pastDues++; %>
-<%      } %> 
-<% } %>
-
-<% for(i = 0; i < taskThrees.length; i++) {%>
-<%      if(taskThrees[i].date === todaysDate.setHours(0,0,0,0)) {%>
-<%          dueToday++; %>
-<%      } %>
-<%      if(taskThrees[i].date < todaysDate.setHours(0,0,0,0)) {%>
-<%          pastDues++; %>
-<%      } %>
-<% } %>
-
-<% for(i = 0; i < taskFours.length; i++) {%>
-<%      if(taskFours[i].date === todaysDate.setHours(0,0,0,0)) {%>
-<%          dueToday++; %>
-<%      } %>
-<%      if(taskFours[i].date < todaysDate.setHours(0,0,0,0)) {%>
-<%          pastDues++; %>
-<%      } %>
-<% } %>
-
-<% for(i = 0; i < taskFives.length; i++) {%>
-<%      if(taskFives[i].date === todaysDate.setHours(0,0,0,0)) {%>
-<%          dueToday++; %>
-<%      } %>
-<%      if(taskFives[i].date < todaysDate.setHours(0,0,0,0)) {%>
-<%          pastDues++; %>
-<%      } %>
-<% } %>
-
-<% for(i = 0; i < taskSixes.length; i++) {%>
-<%      if(taskSixes[i].date === todaysDate.setHours(0,0,0,0)) {%>
-<%          dueToday++; %>
-<%      } %>
-<%      if(taskSixes[i].date < todaysDate.setHours(0,0,0,0)) {%>
-<%          pastDues++; %>
-<%      } %>
-<% } %> -->
+var $toggles = $('.lists').on('click', function() {
+    var checkedStates = $toggles.map(function() {
+      return this.checked;
+    }).get();
+    localStorage.setItem('checkedStates', JSON.stringify(checkedStates));
+    $(this).toggleClass('in');
+  })
+  
+  if (localStorage.getItem('checkedStates')) {
+    JSON.parse(localStorage.getItem('checkedStates')).forEach(function(checked, i) {
+      $('.lists').eq(i).prop('checked', checked).toggleClass('in', checked)
+    });
+  }
