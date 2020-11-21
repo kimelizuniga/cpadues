540aac7e7d68085b2c74e1c2b10f93f10a928aa3
Kim Zuniga <kim.eli.zuniga@gmail.com>
2020-11-20 20:17:23 -0500

    Add login

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const express = require('express'),
       TaskFour = require('./models/classFour'),
       TaskFive = require('./models/classFive'),
       TaskSix = require('./models/classSix'),
-      bodyParser = require('body-parser')
+      User    = require('./models/user'),
+      LocalStrategy             = require('passport-local'),
+      bodyParser = require('body-parser'),
+      passport   = require("passport");
 
 const url = process.env.MONGOURL || 'mongodb://localhost/due'; ;
 
@@ -34,6 +37,21 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride("_method"));
 app.use(flash());
+
+// PASSPORT CONFIG
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
 
 // HOME PAGE
 
@@ -83,6 +101,46 @@ app.get('/', (req, res) => {
         }
     })
 })
+
+// REGISTER
+
+app.get('/register', (req, res) => {
+    res.render("register");
+})
+
+app.post('/register', (req,res) => {
+    req.body.username
+    req.body.password
+    const newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, (err, user) => {
+        if(err){
+            return res.redirect('/register');
+        }
+        req.flash('success', 'Successfully registered - Now logged in as ' + req.body.username)
+        passport.authenticate('local')(req, res, () => {
+            res.redirect('/');
+        })
+    })
+})
+
+app.post('/login', passport.authenticate('local',
+{   
+    failureRedirect: '/',
+    failureFlash: true
+    }), (req, res) => {
+        req.flash('success', 'Successfully logged in');
+        res.redirect('/')
+});
+
+
+//LOGOUT ROUTE
+
+app.get("/logout", function(req, res){
+    req.logout();
+    req.flash("success", "Logged you out");
+    res.redirect("/");
+});
+
 
 // REQUIRE ROUTES
 
